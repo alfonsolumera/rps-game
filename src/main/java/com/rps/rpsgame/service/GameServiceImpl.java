e5cf10cc805dd485d4d2d8c1a9d48fe1ac46cb3f
Alfonso PC <alfonso.lumera@gmail.com>
2020-10-23 20:38:46 +0200

    refactorFolder

--- src/main/java/com/rps/rpsgame/service/GameServiceImpl.java
+++ src/main/java/com/rps/rpsgame/service/GameServiceImpl.java
@@ -1,38 +1,38 @@
 package com.rps.rpsgame.service;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.rps.rpsgame.model.Player;
+import com.rps.rpsgame.utils.Constants;
 
 import org.springframework.stereotype.Service;
 
-import com.rps.rpsgame.constants.Constants;
-import com.rps.rpsgame.model.Player;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service("gameService")
-public class GameServiceImpl implements GameService{
+public class GameServiceImpl implements GameService {
 
-    @Override
-    public List<Player> playRound(Player p1, Player p2) {
+  @Override
+  public List<Player> playRound(Player p1, Player p2) {
 
-        List<Player> game = new ArrayList<>();
-        String scoreP1;
-        String scoreP2;
-        boolean tie = p1.getOption() == p2.getOption();
+    List<Player> game = new ArrayList<>();
+    String scoreP1;
+    String scoreP2;
+    boolean tie = p1.getOption() == p2.getOption();
 
-        if (!tie){
-            scoreP1 = p1.getOption() == Constants.PAPER ? Constants.WINNER : Constants.LOOSER;
-            scoreP2 = Constants.WINNER.equals(scoreP1) ? Constants.LOOSER : Constants.WINNER;
-        }else{
-            scoreP1 = Constants.TIE;
-            scoreP2 = Constants.TIE;
-        }
-
-        p1.getHistoryMatches().add(scoreP1);
-        p2.getHistoryMatches().add(scoreP2);
-
-        game.add(p1);
-        game.add(p2);
-
-        return game;
+    if (!tie) {
+      scoreP1 = p1.getOption() == Constants.PAPER ? Constants.WINNER : Constants.LOOSER;
+      scoreP2 = Constants.WINNER.equals(scoreP1) ? Constants.LOOSER : Constants.WINNER;
+    } else {
+      scoreP1 = Constants.TIE;
+      scoreP2 = Constants.TIE;
     }
+
+    p1.getHistoryMatches().add(scoreP1);
+    p2.getHistoryMatches().add(scoreP2);
+
+    game.add(p1);
+    game.add(p2);
+
+    return game;
+  }
 }
