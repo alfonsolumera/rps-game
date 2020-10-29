67cf725985e9acf36c0439118c1f9645ae06534f
Alfonso PC <alfonso.lumera@gmail.com>
2020-10-29 11:30:50 +0100

    seconds_part

--- src/main/java/com/rps/rpsgame/controller/MenuController.java
+++ src/main/java/com/rps/rpsgame/controller/MenuController.java
@@ -1,14 +1,12 @@
 package com.rps.rpsgame.controller;
 
+import com.rps.rpsgame.model.GameSummary;
 import com.rps.rpsgame.model.SummaryRound;
-import com.rps.rpsgame.model.SummaryRounds;
 import com.rps.rpsgame.service.GameService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -18,19 +16,19 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping({"/", "/index"})
+@SuppressWarnings("unchecked")
 public class MenuController {
 
   @Autowired
   GameService gameService;
 
   @GetMapping(value = "/play")
-  public String play(@ModelAttribute SummaryRounds rounds, Model model,
-      HttpServletRequest request) {
+  public String play(HttpServletRequest request) {
 
     List<SummaryRound> previousRounds =
-        (ArrayList) request.getSession().getAttribute("rounds") != null
-            ? (ArrayList) request.getSession().getAttribute("rounds")
-            : new ArrayList();
+        (ArrayList<SummaryRound>) request.getSession().getAttribute("rounds") != null
+            ? (ArrayList<SummaryRound>) request.getSession().getAttribute("rounds")
+            : new ArrayList<SummaryRound>();
     String totalRounds = (String) request.getSession().getAttribute("roundsPlayed");
 
     List<SummaryRound> lstScored = gameService.playRound(previousRounds);
@@ -40,14 +38,31 @@ public class MenuController {
     request.getSession().setAttribute("roundsPlayed", String.valueOf(currentRound));
     request.getSession().setAttribute("rounds", lstScored);
     return "redirect:/";
+
   }
 
   @GetMapping(value = "/reset")
   public String reset(HttpServletRequest request) {
 
-    request.getSession().setAttribute("rounds", new ArrayList());
+    gameService.saveGameSummary((List<SummaryRound>) request.getSession().getAttribute("rounds"),
+        (String) request.getSession().getAttribute("roundsPlayed"));
+
+    request.getSession().setAttribute("rounds", new ArrayList<SummaryRound>());
     request.getSession().setAttribute("roundsPlayed", "0");
+
     return "redirect:/";
+
+  }
+
+  @GetMapping(value = "/game-summary")
+  public String showSummary(HttpServletRequest request) {
+
+    GameSummary gameSummary = gameService.getSummary();
+
+    request.getSession().setAttribute("gameSummary", gameSummary);
+
+    return "summary";
+
   }
 
 }
--- src/main/java/com/rps/rpsgame/service/GameServiceImpl.java
+++ src/main/java/com/rps/rpsgame/service/GameServiceImpl.java
@@ -1,5 +1,6 @@
 package com.rps.rpsgame.service;
 
+import com.rps.rpsgame.model.GameSummary;
 import com.rps.rpsgame.model.OptionsModel;
 import com.rps.rpsgame.model.Player;
 import com.rps.rpsgame.model.SummaryRound;
@@ -10,9 +11,14 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class GameServiceImpl implements GameService {
+
+  private GameSummary gameSummary = new GameSummary();
 
   @Override
   public List<SummaryRound> playRound(List<SummaryRound> summary) {
@@ -31,12 +37,40 @@ public class GameServiceImpl implements GameService {
     }
 
     if (CollectionUtils.isEmpty(summary)) {
-      summary = round;
-    } else {
-      summary.addAll(round);
+      return round;
     }
 
+    summary.addAll(round);
     return summary;
+
+
   }
+
+  @Override
+  public void saveGameSummary(List<SummaryRound> previousRounds, String totalRounds) {
+
+    Map<SummaryRound, Long> ocurrencias = previousRounds.stream()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+    this.gameSummary.setTotalWinsOnePlayer(
+        ocurrencias.entrySet().stream().filter(s -> "PLAYER 1".equals(s.getKey().getName()))
+            .findFirst().map(Map.Entry::getValue).orElse((long) 0));
+    this.gameSummary.setTotalWinsSecondPlayer(
+        ocurrencias.entrySet().stream().filter(s -> "PLAYER 2".equals(s.getKey().getName()))
+            .findFirst().map(Map.Entry::getValue).orElse((long) 0));
+    this.gameSummary.setTotalDraws(
+        ocurrencias.entrySet().stream().filter(s -> "TIE".equals(s.getKey().getName())).findFirst()
+            .map(Map.Entry::getValue).orElse((long) 0));
+    this.gameSummary
+        .setTotalRounds(this.gameSummary.getTotalRounds() + Integer.valueOf(totalRounds));
+
+    System.out.println();
+  }
+
+  @Override
+  public GameSummary getSummary() {
+    return this.gameSummary;
+  }
+
 
 }
