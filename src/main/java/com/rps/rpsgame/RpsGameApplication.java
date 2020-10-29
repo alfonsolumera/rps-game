f46be8b0acebe65836a17fb6e310c77b8b5d1e49
Alfonso PC <alfonso.lumera@gmail.com>
2020-10-29 16:29:16 +0100

    pageError

--- src/main/java/com/rps/rpsgame/RpsGameApplication.java
+++ src/main/java/com/rps/rpsgame/RpsGameApplication.java
@@ -2,12 +2,19 @@ package com.rps.rpsgame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RpsGameApplication {
+public class RpsGameApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RpsGameApplication.class, args);
-	}
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(RpsGameApplication.class);
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(RpsGameApplication.class, args);
+  }
 
 }
